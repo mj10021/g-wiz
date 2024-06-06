bff6f770c26a74a580354529ec9bb00dce7ab0c0
James Dietz <jamesthespeedy@gmail.com>
2024-06-06 15:18:39 -0400

    adding gcode resource

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, transform::commands};
+use print_analyzer::{Parsed, Uuid, Pos};
+
+#[derive(Resource)]
+struct GCode(Parsed);
 
 #[derive(Component)]
 struct Extrusion {
+    id: Uuid,
     xi: f32,
     yi: f32,
     zi: f32,
@@ -11,23 +16,29 @@ struct Extrusion {
     e: f32
 }
 impl Extrusion {
-    fn new() -> Extrusion {
-        Extrusion {
-            xi: 0.0,
-            yi: 0.0,
-            zi: 0.0,
-            xf: 0.0,
-            yf: 0.0,
-            zf: 0.0,
-            e: 0.0
-        }
+    fn from_vertex(gcode: &Parsed, vertex: &Uuid) -> Extrusion {
+        let v = gcode.vertices.get(vertex).unwrap();
+        let prev = gcode.vertices.get(&v.prev.unwrap()).unwrap();
+        let Pos {x: xi, y: yi, z: zi, ..} = prev.to;
+        let Pos {x: xf, y: yf, z: zf, e, ..} = v.to;
+        Extrusion { id: v.id, xi, yi, zi, xf, yf, zf, e }
     }
 }
-fn draw_extrustions(mut commands: Commands) {
-    commands.spawn(Extrusion::new());
+fn load_gcode(gcode: Res<GCode>, mut commands: Commands) {
+
+}
+fn draw_extrustions(gcode: Res<GCode>, mut commands: Commands) {
+    let gcode = &gcode.0;
+    for vertex in gcode.vertices.keys() {
+        commands.spawn(Extrusion::from_vertex(&gcode, vertex));
+    }
 }
 fn main() {
+    let gcode = Parsed::new();
     App::new()
+        .insert_resource(GCode(gcode))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, load_gcode)
         .add_systems(Update, draw_extrustions)
         .run();
 }
