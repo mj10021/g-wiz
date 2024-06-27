e2e0d27b2500a14e171b9d435e90e1159fcb8f41
James Dietz <jamesthespeedy@gmail.com>
2024-06-27 16:27:00 -0400

    undo the parent child thing on render

--- src/render.rs
+++ src/render.rs
@@ -2,28 +2,38 @@ use super::{
     print_analyzer::Label, settings::*, ForceRefresh, GCode, IdMap, PickableBundle, Tag, UiResource,
 };
 use bevy::prelude::*;
-use super::Id;
 
-#[derive(Bundle)]
-struct ExtrusionParent {
-    pickable: PickableBundle,
-    tag: Tag,
-    spatial: SpatialBundle
-}
-
-impl ExtrusionParent {
-    fn spawn(
-        commands: &mut Commands,
-        start: Vec3,
-        end: Vec3,
-        flow: f32,
-        id: Id,
-        meshes: &mut ResMut<Assets<Mesh>>,
-        materials: &mut ResMut<Assets<StandardMaterial>>,
-        settings: &Res<Settings>
-    ) -> Entity {
-        let parent = commands.spawn(ExtrusionParent {pickable: PickableBundle::default(), tag: Tag{id}, spatial: SpatialBundle::default()}).id();
-
+pub fn render(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut map: ResMut<IdMap>,
+    gcode: Res<GCode>,
+    shapes: Query<Entity, With<Tag>>,
+    settings: Res<Settings>,
+) {
+    for shape in shapes.iter() {
+        commands.entity(shape).despawn();
+    }
+    let gcode = &gcode.0;
+    let mut pos_list = Vec::new();
+    for v in gcode.vertices.values() {
+        let (xf, yf, zf) = (v.to.x, v.to.y, v.to.z);
+        let (xi, yi, zi) = {
+            if let Some(prev) = v.prev {
+                let p = gcode.vertices.get(&prev).unwrap();
+                (p.to.x, p.to.y, p.to.z)
+            } else {
+                (0.0, 0.0, 0.0)
+            }
+        };
+        let (start, end) = (Vec3::new(xi, yi, zi), Vec3::new(xf, yf, zf));
+        let dist = start.distance(end);
+        let flow = v.to.e / dist;
+        pos_list.push((v.id, start, end, flow));
+    }
+    for (id, start, end, flow) in pos_list {
+        // Create a cylinder mesh
         let radius = (flow / std::f32::consts::PI).sqrt();
 
         // Create the mesh and material
@@ -56,65 +66,37 @@ impl ExtrusionParent {
         let direction = end - start;
         let rotation = Quat::from_rotation_arc(Vec3::Y, direction.normalize());
         // Add the cylinder to the scene
-        let line = commands.spawn( PbrBundle {
-            mesh: mesh_handle,
-            material: material_handle,
-            transform: Transform {
-                translation: middle,
-                rotation,
-                ..Default::default()
-            },
-            ..Default::default()
-        }).id();
+        let e_id = commands
+            .spawn((
+                PbrBundle {
+                    mesh: mesh_handle,
+                    material: material_handle,
+                    transform: Transform {
+                        translation: middle,
+                        rotation,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                PickableBundle::default(),
+                Tag { id },
+            ))
+            .id();
         // add the
-        let end = commands.spawn(PbrBundle {
-            mesh: sphere,
-            material: sphere_material,
-            transform: Transform {
-                translation: end,
+        commands.spawn((
+            PbrBundle {
+                mesh: sphere,
+                material: sphere_material,
+                transform: Transform {
+                    translation: end,
+                    ..Default::default()
+                },
                 ..Default::default()
             },
-            ..Default::default()
-        }).id();
-        commands.entity(parent).push_children(&[line, end]);
-        parent
-    }
-}
-
-pub fn render(
-    mut commands: Commands,
-    mut map: ResMut<IdMap>,
-    gcode: Res<GCode>,
-    shapes: Query<Entity, With<Tag>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    settings: Res<Settings>,
-
-) {
-    for shape in shapes.iter() {
-        commands.entity(shape).despawn();
-    }
-    let gcode = &gcode.0;
-    let mut pos_list = Vec::new();
-    for v in gcode.vertices.values() {
-        let (xf, yf, zf) = (v.to.x, v.to.y, v.to.z);
-        let (xi, yi, zi) = {
-            if let Some(prev) = v.prev {
-                let p = gcode.vertices.get(&prev).unwrap();
-                (p.to.x, p.to.y, p.to.z)
-            } else {
-                (0.0, 0.0, 0.0)
-            }
-        };
-        let (start, end) = (Vec3::new(xi, yi, zi), Vec3::new(xf, yf, zf));
-        let dist = start.distance(end);
-        let flow = v.to.e / dist;
-        pos_list.push((v.id, start, end, flow));
-    }
-    for (id, start, end, flow) in pos_list {
-        // Create a cylinder mesh
-        let b_id = ExtrusionParent::spawn(&mut commands, start, end, flow, id, &mut meshes, &mut materials, &settings);
-        map.0.insert(id, b_id);
+            PickableBundle::default(),
+            Tag { id },
+        ));
+        map.0.insert(id, e_id);
     }
     commands.remove_resource::<ForceRefresh>();
 }
