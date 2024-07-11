e4200231d795e706edc70090aeec7dba9a5b17cd
James Dietz <jamesthespeedy@gmail.com>
2024-07-11 09:55:37 -0400

    fixup

--- src/select.rs
+++ src/select.rs
@@ -8,9 +8,7 @@ pub fn select_deselect_all(
     mut selections: Query<&mut PickSelection>,
     select_all: Res<SelectAll>,
 ) {
-    for mut selection in selections.iter_mut() {
-        selection.is_selected = select_all.0;
-    }
+    selections.iter_mut().map(|mut s| s.is_selected = select_all.0);
 }
 
 #[derive(Default, Resource, Clone, PartialEq, Hash)]
