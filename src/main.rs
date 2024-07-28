6c2be0f1740069f093901c7feac94340c13e9f4e
James Dietz <jamesthespeedy@gmail.com>
2024-07-28 19:38:44 -0400

    asdf

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod ui;
 use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
 use bevy_mod_picking::prelude::*;
-use events::events::*;
+use events::*;
 use events::handlers::*;
 use history::{undo_redo_selections, update_selection_log, SelectionLog};
 use pan_orbit::{pan_orbit_camera, PanOrbitCamera};
